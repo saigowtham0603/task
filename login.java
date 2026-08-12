56f31342cf3212574c4635868e859119a4478a68
gowtham <saigowthamnakkala@gmail.com>
2026-08-12 17:00:44 +0530

    updated login in main

--- login.java
+++ login.java
@@ -1,5 +1,18 @@
-public class login {
+import java.util.*;
+
+public class Login {
     public static void main(String[] args) {
-        System.out.println("Login Page");
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Username: ");
+        String u = s.nextLine();
+
+        System.out.print("Password: ");
+        String p = s.nextLine();
+
+        if (u.equals("admin") && p.equals("1234"))
+            System.out.println("Login Successful");
+        else
+            System.out.println("Invalid Login");
     }
 }
