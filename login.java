78af29488dfee76083c606fbc7e13dfefac34e69
gowtham <saigowthamnakkala@gmail.com>
2026-08-12 16:58:59 +0530

    updated login in feature

--- login.java
+++ login.java
@@ -1,5 +1,30 @@
-public class login {
+import java.util.Scanner;
+
+public class Login {
+
     public static void main(String[] args) {
-        System.out.println("Login Page");
+
+        Scanner sc = new Scanner(System.in);
+
+        String username = "admin";
+        String password = "1234";
+
+        System.out.println("===== LOGIN =====");
+
+        System.out.print("Enter Username: ");
+        String u = sc.nextLine();
+
+        System.out.print("Enter Password: ");
+        String p = sc.nextLine();
+
+        if (u.equals(username) && p.equals(password)) {
+            System.out.println("\nLogin Successful!");
+            System.out.println("Welcome, " + u);
+            System.out.println("Opening Dashboard...");
+        } else {
+            System.out.println("\nInvalid username or password!");
+        }
+
+        sc.close();
     }
 }
